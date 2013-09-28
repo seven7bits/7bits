cfd1a080f28025ad0cdaa48418b1b2f0797a81a8
msemenistyi <nikeiwe@gmail.com>
2013-09-28 17:13:22 +0300

    switch to jade templates

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 var express = require('express')
+  , jade    = require('jade')
   , path    = require('path');
 
 var port = 3000;
 
 var app = express();
+
+app.configure(function(){
+	app.set('views', __dirname + '/../views');
+	app.set('view engine', 'jade');
+});
+
+
+app.get('/', function(req, res){
+	res.render('index', {title: 'Main'});
+});
+
+app.get('/:token', function(req, res){
+	var token = req.params.token;
+	res.render('room', {token: token});
+});
+
 app.use(express.static(__dirname + '/../app'));
 app.listen(port);
