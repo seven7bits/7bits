0cf1117400abf6579bd91762ca9efd50d728a960
boo1ean <boo1ean0807@gmail.com>
2013-09-28 19:31:51 +0300

    Add gamepad key mappings

---
+++ app/js/lib/gamepad.js
@@ -0,0 +1,18 @@
+define(function(require) {
+	var k = {
+		LEFT   : 0,
+		RIGHT  : 1,
+		TOP    : 2,
+		DOWN   : 3,
+		A      : 4,
+		B      : 5,
+		AA     : 6,
+		BB     : 7,
+		START  : 8,
+		SELECT : 9
+	}
+
+	var gamepad = {};
+
+	return gamepad;
+});
