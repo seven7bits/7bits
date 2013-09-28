f5721665b9a347311e8fd11dfdc1491fa4d86356
boo1ean <boo1ean0807@gmail.com>
2013-09-28 22:04:40 +0300

    Add gamepad proxy object

--- app/js/lib/gamepad.js
+++ app/js/lib/gamepad.js
@@ -1,18 +1,46 @@
 define(function(require) {
-	var k = {
-		LEFT   : 0,
-		RIGHT  : 1,
-		TOP    : 2,
-		DOWN   : 3,
-		A      : 4,
-		B      : 5,
-		AA     : 6,
-		BB     : 7,
-		START  : 8,
-		SELECT : 9
-	}
+	var io = require('io');
+	return {
+		ioServer: 'http://127.0.0.1:3000',
+		config: null,
 
-	var gamepad = {};
+		room: function() {
+			return window.location.href.split('/').pop();
+		},
 
-	return gamepad;
+		configure: function(config) {
+			this.config = config;
+		},
+
+		start: function() {
+			if (!this.config) {
+				throw new Error('Key mapping config is required.');
+			}
+
+			this.listen();
+		},
+
+		listen: function() {
+			var that = this;
+			this.io = io.connect(this.ioServer);
+
+			this.io.on('connect', function() {
+				that.io.emit('room', that.room);
+			});
+
+			this.io.on('a', function(a) {
+				this.trigger(a.player, a.key);
+			});
+		},
+
+		trigger: function(player, key) {
+			var c = this.config[player][key];
+
+			var method  = c.method ? c.method : this.config.base.method;
+			var context = c.context ? c.context : this.config.base.context;
+			var args    = c.args ? c.args : this.config.base.args;
+
+			method.apply(context, args);
+		}
+	};
 });
