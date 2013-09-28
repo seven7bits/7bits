c7c87f6bd33456014c4b726fb33f8dcd638fc53e
boo1ean <boo1ean0807@gmail.com>
2013-09-29 02:38:42 +0300

    Add rooms, update nes config

--- app/js/room/views/game-area.js
+++ app/js/room/views/game-area.js
@@ -4,8 +4,35 @@ define(function(require) {
 		onShow: function() {
 			var ui = $('#game-area').JSNESUI({
 				"Homebrew": [
-					['Concentration Room', '/vendor/jsnes/roms/croom/croom.nes'],
-					['LJ65', '/vendor/jsnes/roms/lj65/lj65.nes']
+					['Concentration Room', 'roms/croom/croom.nes'],
+					['LJ65', 'roms/lj65/lj65.nes'],
+					['Bomberman', 'roms/BM/BM.nes'],
+					['Captain American','roms/CA/Captain American [p1][!].nes'],
+					['Flintstones','roms/FS/Flintstones, The - The Surprise at Dinosaur Peak! (U) [p1][!].nes'],
+					['Megaman II','roms/G/Megaman II (U) [!].nes'],
+					['Double Dragon','roms/Q/Double Dragon (U) [!].nes'],
+					['F-1 Race','roms/W/F-1 Race (J).nes']
+				],
+
+				"Working": [
+					['Bubble Bobble', 'roms/local-roms/Bubble Bobble (U).nes'],
+
+					['Contra', 'roms/local-roms/Contra (U) [!].nes'],
+					['Donkey Kong', 'roms/local-roms/Donkey Kong (JU).nes'],
+					['Dr. Mario', 'roms/local-roms/Dr. Mario (JU).nes'],
+					['Golf', 'roms/local-roms/Golf (JU).nes'],
+					['The Legend of Zelda', 'roms/local-roms/Legend of Zelda, The (U) (PRG1).nes'],
+					['Lemmings', 'roms/local-roms/Lemmings (U).nes'],
+					['Lifeforce', 'roms/local-roms/Lifeforce (U).nes'],
+
+					['Mario Bros.', 'roms/local-roms/Mario Bros. (JU) [!].nes'],
+					['Mega Man', 'roms/local-roms/Mega Man (U).nes'],
+					['Pac-Man', 'roms/local-roms/Pac-Man (U) [!].nes'],
+					['Super Mario Bros.', 'roms/local-roms/Super Mario Bros. (JU) (PRG0) [!].nes'],
+					['Tennis', 'roms/local-roms/Tennis (JU) [!].nes'],
+					['Tetris', 'roms/local-roms/Tetris (U) [!].nes'],
+					['Tetris 2', 'roms/local-roms/Tetris 2 (U) [!].nes'],
+					['Zelda II - The Adventure of Link', 'roms/local-roms/Zelda II - The Adventure of Link (U).nes']
 				]
 			});
 
