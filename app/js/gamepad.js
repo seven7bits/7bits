dfd63edd99dc9cfd647cdda1498aef770bc662f5
msemenistyi <nikeiwe@gmail.com>
2013-09-29 07:45:40 +0300

    diagonal keyboard enable

--- app/js/gamepad.js
+++ app/js/gamepad.js
@@ -27,28 +27,61 @@
 
 	function initializeBindings(){
 		var controlsList = ['up', 'down', 'left', 'right', 'select', 'start', 'full-screen', 'a', 'b', 'aa', 'bb'];
-		var controlsEls = [];
+		var sharedControls = ['up-left','up-right','down-left','down-right'];
 
 		for (var i = 0; i < controlsList.length; i++) {
 			var id = controlsList[i]; 
-			var el = document.getElementById(id);
 			$$('#' + id).on('touchstart', onTouchStart);
 			$$('#' + id).on('touchend', onTouchEnd);
+			$$('#' + id).on('mousedown', onTouchStart);
+			$$('#' + id).on('mouseup', onTouchEnd);
 		}
+		
+		for (var i = 0; i < sharedControls.length; i++) {
+			var id = sharedControls[i]; 
+			$$('#' + id).on('touchstart', function(ev){ onTouchStart(ev, {isShared: true});});
+			$$('#' + id).on('touchend', function(ev){ onTouchEnd(ev, {isShared: true});});
+			$$('#' + id).on('mousedown', function(ev){ onTouchStart(ev, {isShared: true});});
+			$$('#' + id).on('mouseup', function(ev){ onTouchEnd(ev, {isShared: true});});
+		}
+
 		$$('body').swipeRight(function(ev){toggleMenu(ev, true);});
 		$$('body').swipeLeft(function(ev){toggleMenu(ev, false);});
 	}
 
 
-	function onTouchStart(ev) {
-		ev.currentTarget.className = 'mousedown';
-		socketConnection.emit('a', {k: ev.currentTarget.id, s: 1});
+	function onTouchStart(ev, options) {
+		if (typeof options === 'object' && options.isShared){
+			var ids = ev.currentTarget.id.split('-');	
+			for (var i = 0; i < ids.length; i++){
+				activateEl(document.getElementById(ids[i]));
+			}
+		} else {
+			activateEl(ev.currentTarget);
+		}
+
+		function activateEl(obj){
+			obj.className = 'mousedown';
+			socketConnection.emit('a', {k: obj.id, s: 1});
+		}
 	}
 
-	function onTouchEnd(ev) {
-		ev.currentTarget.className = '';
-		socketConnection.emit('a', {k: ev.currentTarget.id, s: 0});
+	function onTouchEnd(ev, options) {
+		if (typeof options === 'object' && options.isShared){
+			var ids = ev.currentTarget.id.split('-');
+			for (var i = 0; i < ids.length; i++){
+				deactivateEl(document.getElementById(ids[i]));
+			}
+		} else {
+			deactivateEl(ev.currentTarget);
+		}
+
+		function deactivateEl(obj){
+			obj.className = '';
+			socketConnection.emit('a', {k: obj.id, s: 0});
+		}
 	}
+
 
 	function toggleMenu(ev, isShown) {
 		if (isShown){
